21edb3726aff1ee5dcee28a23311f00fc5bb4bfe
zulfiqarAlibalti <zulfibalti592@gmail.com>
2024-02-13 19:40:00 +0500

    edited link

--- portfolio.ts
+++ portfolio.ts
@@ -17,7 +17,7 @@ export const greetings: GreetingsType = {
   description:
     "An adept Data Scientist with a robust foundation in Applied and Computational Mathematics. My expertise spans Machine Learning, Deep Learning, Image Processing, and Signal Processing. I leverage Natural Language Processing to optimize outcomes and excel in developing End-to-End Machine Learning and Computer Vision applications.",
   resumeLink:
-    "https://github.com/zulfiqarAlibalti/Resume.git",
+    "https://github.com/zulfiqarAlibalti/Resume/blob/master/Resume.pdf",
 };
 
 export const openSource = {
